44b5953b4d729ad4a1e64087c3d58724460a5d55
Cristian Cucu <xxxcucus@yahoo.com>
2018-10-27 09:08:19 +0200

    Organized histograms in 2d array structure.

--- cudaMPbTools/cudaimage.cu
+++ cudaMPbTools/cudaimage.cu
@@ -1,33 +1,81 @@
 #include "cudaimage.h"
 
-CudaImage::CudaImage(unsigned char* image_data, int image_width, int image_height) : 
-	m_Width(image_width), m_Height(image_height)
+CudaImage::CudaImage(unsigned char* image_data, int image_width, int image_height, int scale) : 
+	m_Width(image_width), m_Height(image_height), m_Scale(scale)
 {
-	m_LastCudaError = cudaMalloc(&m_dSourceImage, image_width * image_height);
-
-	if (m_LastCudaError == cudaSuccess) {
-		m_LastCudaError = cudaMemcpy(image_data, m_dSourceImage, image_width * image_height, cudaMemcpyDeviceToHost);
-	}
-	else {
-		m_dSourceImage = nullptr;
-		return;
-	}
-
-	m_LastCudaError = cudaMalloc(&m_GradientImages, m_ArcNo * image_width * image_height * sizeof(double));
+	//copy image to the device memory and pad with zeros
+	//TODO: for start pad with zeros, but later as in the CPU method
+	//another solution is to pad the image before it is given to this class
+	m_LastCudaError = cudaMalloc(&m_dSourceImage, (image_width + 2 * m_Scale) * (image_height + 2 * m_Scale));
 
 	if (m_LastCudaError != cudaSuccess)
 		return;
 
-	m_LastCudaError = cudaMemcpy(m_GradientImages, m_ArcNo * image_width * image_height * sizeof(double));
+	//set all pixels in the image to zero 
+	m_LastCudaError = cudaMemset(m_dSourceImage, 0, (image_width + 2 * m_Scale) * (image_height + 2 * m_Scale));
 
 	if (m_LastCudaError != cudaSuccess)
 		return;
+
+	//copy from the host image with padding
+	int count = 0;
+	while (count < image_height && m_LastCudaError == cudaSuccess)
+	{
+		m_LastCudaError = cudaMemcpy(image_data + count * image_width, m_dSourceImage + m_Scale * (image_width + 2 * m_Scale) + count * (image_width + 2 * m_Scale) + m_Scale, image_width, cudaMemcpyHostToDevice);
+		count++;
+	}
+
+	if (m_LastCudaError != cudaSuccess)
+		return;	
+
+	//allocate the device memory for the gradient images
+	m_LastCudaError = cudaMalloc(&m_dGradientImages, m_ArcNo * image_width * image_height * sizeof(double));
+
+	if (m_LastCudaError != cudaSuccess) {
+		m_dGradientImages = nullptr;
+		return;
+	}
+
+	//set all pixels in the gradient images to 0
+	m_LastCudaError = cudaMemset(m_dGradientImages, 0, m_ArcNo * image_width * image_height * sizeof(double));
+
+	if (m_LastCudaError != cudaSuccess)
+		return;
+
+	//preparing histograms
+	m_LastCudaError = cudaMalloc(&m_dHistograms, (m_Height + 2 * m_Scale) * sizeof(unsigned char*));
+	for (int i = 0; i < m_Height + 2 * m_Scale; ++i)
+		m_dHistograms[i] = nullptr;
 
 	m_FullyInitialized = true;
 }
 
+bool CudaImage::initializeHistoRange(int start, int stop)
+{ 
+    for (int i = start; i < stop; ++i) {
+		m_LastCudaError = cudaMalloc(&m_dHistograms[i], 256 * 2 * m_ArcNo * (m_Width + 2 * m_Scale) * sizeof(unsigned char));
+		
+		if (m_LastCudaError != cudaSuccess)
+			return false;
+
+		//set all pixels in the gradient images to 0
+		m_LastCudaError = cudaMemset(m_dHistograms[i], 0, 256 * 2 * m_ArcNo * (m_Width + 2 * m_Scale) * sizeof(unsigned char));
+
+		if (m_LastCudaError != cudaSuccess)
+			return false;
+		}
+
+	return true;
+}
 
 CudaImage::~CudaImage()
 {
 	cudaFree(m_dSourceImage);
+	cudaFree(m_dGradientImages);
+	
+	for (int i = 0; i < m_Height + 2 * m_Scale; ++i)
+		if (m_dHistograms[i] != nullptr)
+			cudaFree(m_dHistograms[i]);
+
+	cudaFree(m_dHistograms);
 }
--- cudaMPbTools/cudaimage.h
+++ cudaMPbTools/cudaimage.h
@@ -3,19 +3,30 @@
 
 class CudaImage {
 public:
-	CudaImage(unsigned char* image_data, int image_width, int image_height);
+	CudaImage(unsigned char* image_data, int image_width, int image_height, int scale);
 	~CudaImage();
 
 	bool wasSuccessfullyCreated() {
 		return m_LastCudaError == cudaSuccess;
 	}
 
+    /**
+     * @brief initializeHistoRange - creates empty histograms for rows of image
+     * between start and stop.
+     * @param start
+     * @param stop
+     */
+	//TODO: to implement
+    bool initializeHistoRange(int start, int stop);	
+
 private:
 	unsigned char* m_dSourceImage = nullptr; //image on the GPU
-	double* m_GradientImages = nullptr; //the result images
+	double* m_dGradientImages = nullptr; //the result images
+	unsigned char** m_dHistograms = nullptr;
 
 	int m_Width = 0;
 	int m_Height = 0;
+	int m_Scale = 0;
 
 	cudaError_t m_LastCudaError;
 
