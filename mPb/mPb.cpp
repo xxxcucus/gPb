e3bfc7a1639c7ed8997948fd464e470f7a17e9ba
Cristian Cucu <xxxcucus@yahoo.com>
2018-12-09 22:36:01 +0100

    Create mPb class.

--- mPb/mPb.cpp
+++ mPb/mPb.cpp
@@ -1,1 +1,41 @@
 #include "mPb.h"
+
+#include "texton.h"
+#include "textontools.h"
+
+MultiscalePb::MultiscalePb(cv::Mat img, const std::string& textonsPath): 
+	m_OrigImage(img), m_TextonPath(textonsPath) {
+	calculateComponentImages();
+}
+
+bool MultiscalePb::computeGradients() {
+
+	return true;
+}
+
+void MultiscalePb::calculateComponentImages() {
+	cv::Mat imgLab;
+	cv::cvtColor(m_OrigImage, imgLab, CV_BGR2Lab);
+	std::vector<cv::Mat> imgLabComp;
+	cv::split(imgLab, imgLabComp);
+	cv::Mat lImage = imgLabComp[0];
+	cv::Mat aImage = imgLabComp[1];
+	cv::Mat bImage = imgLabComp[2];
+
+	std::vector<Texton> textons;
+	if (!TextonTools::readFromTextonsFile(m_TextonPath, textons)) {
+		printf("Could not read the textons \n");
+		exit(1);
+	}
+
+	cv::Mat textonImage;
+	if (!TextonTools::convertToTextonImage(m_OrigImage, textons, textonImage)) {
+		printf("Error when converting to texton image\n");
+		exit(1);
+	}
+
+	m_ComponentImages["a"] = aImage;
+	m_ComponentImages["b"] = bImage;
+	m_ComponentImages["l"] = lImage;
+	m_ComponentImages["t"] = textonImage;
+}
--- mPb/mPb.h
+++ mPb/mPb.h
@@ -1,7 +1,55 @@
 #ifndef __MULTISCALE_PB_
 #define __MULTISCALE_PB_
 
+#include "opencv2/opencv.hpp"
+#include <map>
+#include <vector>
 
+class MultiscalePb {
+
+	/**
+	* img must be RGB image
+	*/
+	MultiscalePb(cv::Mat img, const std::string& textonPath);
+
+	void setScales(const std::string& comp, const std::vector<int>& scales) {
+		m_Scales[comp] = scales;
+	}
+
+	/**
+	* Computes the gradient images for all scales, orientations, and components
+	*/
+	bool computeGradients();
+
+private:
+	void calculateComponentImages();
+
+private:
+
+	cv::Mat m_OrigImage;
+
+	/**
+	* Path to quantized textons.
+	*/
+	std::string m_TextonPath;
+
+	std::map<std::string, cv::Mat> m_ComponentImages;
+
+	/**
+	* For each component and each orientation a vector of gradient images, one for each scale
+	*/
+	std::map <std::string, std::vector<std::vector<cv::Mat>>> m_GradientImages;
+	/**
+	* For each component and each orientation a vector of weights, one for each scale
+	*/
+	std::map<std::string, std::vector<std::vector<double>>> m_Alphas;
+	/**
+	* For each component a vector of the scales used. 
+	*/
+	std::map<std::string, std::vector<int>> m_Scales;
+	const std::vector<std::string> m_ComponentNames = { "a", "b", "l", "t" };
+	const std::vector<double> m_Orientations = { 0, CV_PI / 4.0, CV_PI / 2.0, CV_PI * 3.0 / 4.0 };
+};
 
 
 
